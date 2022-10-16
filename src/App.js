bfd1d1e6bdafdb78481e28c46fbaf546136709fa
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-16 12:54:04 +0100

    chore: import menu, category, item to app js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import "./App.css";
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
-  return <div className="App"></div>;
+  return <h2>menu project setup</h2>;
 }
 
 export default App;
